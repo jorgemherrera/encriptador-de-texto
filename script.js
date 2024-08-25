188dcbadedb74e89a37009791825ec90adad9c00
Jorge Herrera <jorgeherrerag12@gmail.com>
2024-08-25 13:37:22 -0400

    Primera versión del encriptador de texto

---
+++ script.js
@@ -0,0 +1,28 @@
+function encriptar() {
+    let texto = document.getElementById("inputText").value;
+    let textoEncriptado = texto
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    document.getElementById("outputText").value = textoEncriptado;
+}
+
+function desencriptar() {
+    let texto = document.getElementById("inputText").value;
+    let textoDesencriptado = texto
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    document.getElementById("outputText").value = textoDesencriptado;
+}
+
+function copiar() {
+    let texto = document.getElementById("outputText");
+    texto.select();
+    document.execCommand("copy");
+    alert("Texto copiado al portapapeles!");
+}
